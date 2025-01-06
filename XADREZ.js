61178f39ce61c5557edcbb28144d82f959e36148
Éric Gomes <100045155+ericGL26@users.noreply.github.com>
2025-01-06 13:00:30 -0300

    Add files via upload

--- XADREZ.js
+++ XADREZ.js
@@ -22,25 +22,33 @@ function GerarTabuleiro() {
         cor = "branca"
       }
     }
-
   }
 }
 
-
 function ColocarPecasTabuleiro(){
+  const pecasXadrez = ['torre', 'cavalo', 'bispo', 'rainha', 'rei', 'bispo', 'cavalo', 'torre']
   for(var y = 0; y < 8; y++){
     for(var x = 0; x < 8; x++){
       var casas = document.getElementById(`${alfabeto[y]}${x+1}`)
-      if(casas.id.slice(1) == 2||casas.id.slice(1) == 7){
-        casas.innerHTML += `<img src="peao.png" class="peao">`
+      var casasUltimoNumero = casas.id.slice(1)
+
+      if(casasUltimoNumero == 2||casasUltimoNumero == 7){
+        casas.innerHTML += `<img src="peao.png" class="peca">`
+      }else if(casasUltimoNumero == 1){
+        casas.innerHTML += `<img id="${pecasXadrez[y] + casas.id}" src="${pecasXadrez[y] + '.png'}" class="peca">`
+        console.log('Casaspeca', pecasXadrez[y] + casas.id)
+      }else if(casasUltimoNumero == 8) {
+        casas.innerHTML += `<img id="${pecasXadrez[y] + casas.id}" src="${pecasXadrez[y] + '.png'}" class="peca">`
+        console.log('Casaspeca2', pecasXadrez[y] + casas.id)
       }
-      console.log('casaslength', casas.id.slice(1))
     }
   }
 }
 
 
-
+function MovimentarPecas(){
+  
+}
 
 GerarTabuleiro()
 ColocarPecasTabuleiro()
