7b6790c697a53292ad29afe88adf75a04e628ef7
Éric Gomes <100045155+ericGL26@users.noreply.github.com>
2025-01-07 18:07:47 -0300

    Add files via upload

--- XADREZ.js
+++ XADREZ.js
@@ -1,6 +1,7 @@
 const tabuleiro = document.getElementById("tabuleiro");
 
 const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const pecasXadrez = ['torre', 'cavalo', 'bispo', 'rainha', 'rei', 'bispo', 'cavalo', 'torre']
 
 function GerarTabuleiro() {
   for(var i = 9; i > 1; i--){
@@ -15,10 +16,12 @@ function GerarTabuleiro() {
 
     for(var j = 0; j < 8; j++){
       if(cor == "branca"){
-        linha.innerHTML += `<div id="${alfabeto[j]}${i-1}" class="quadrado ${cor}">${alfabeto[j]}${i-1}</div>`;
+        var casaclicada = `${alfabeto[j]}${i-1}`
+        linha.innerHTML += `<button onclick="MovimentarPecas(${casaclicada})" id="${alfabeto[j]}${i-1}" class="quadrado ${cor}">${alfabeto[j]}${i-1}</button>`;
         cor = "preta"
       }else if(cor == "preta"){
-        linha.innerHTML += `<div id="${alfabeto[j]}${i-1}" class="quadrado ${cor}">${alfabeto[j]}${i-1} </div>`;
+        var casaclicada = `${alfabeto[j]}${i-1}`
+        linha.innerHTML += `<button onclick="MovimentarPecas(${casaclicada})" id="${alfabeto[j]}${i-1}" class="quadrado ${cor}">${alfabeto[j]}${i-1} </button>`;
         cor = "branca"
       }
     }
@@ -26,7 +29,6 @@ function GerarTabuleiro() {
 }
 
 function ColocarPecasTabuleiro(){
-  const pecasXadrez = ['torre', 'cavalo', 'bispo', 'rainha', 'rei', 'bispo', 'cavalo', 'torre']
   for(var y = 0; y < 8; y++){
     for(var x = 0; x < 8; x++){
       var casas = document.getElementById(`${alfabeto[y]}${x+1}`)
@@ -35,21 +37,27 @@ function ColocarPecasTabuleiro(){
       if(casasUltimoNumero == 2||casasUltimoNumero == 7){
         casas.innerHTML += `<img src="peao.png" class="peca">`
       }else if(casasUltimoNumero == 1){
-        casas.innerHTML += `<img id="${pecasXadrez[y] + casas.id}" src="${pecasXadrez[y] + '.png'}" class="peca">`
-        console.log('Casaspeca', pecasXadrez[y] + casas.id)
+        casas.innerHTML += `<img id="${pecasXadrez[y]}${alfabeto[y]}${x+1}" class="peca">`
+        const peca = document.getElementById(`${pecasXadrez[y]}${alfabeto[y]}${x+1}`)
+        peca.setAttribute("src", pecasXadrez[y] + '.png')
       }else if(casasUltimoNumero == 8) {
-        casas.innerHTML += `<img id="${pecasXadrez[y] + casas.id}" src="${pecasXadrez[y] + '.png'}" class="peca">`
-        console.log('Casaspeca2', pecasXadrez[y] + casas.id)
+        casas.innerHTML += `<img id="${pecasXadrez[y]}${alfabeto[y]}${x+1}" class="peca">`
+        var peca = document.getElementById(`${pecasXadrez[y]}${alfabeto[y]}${x+1}`)
+        peca.setAttribute("src", pecasXadrez[y] + '.png')
       }
     }
   }
 }
 
-
-function MovimentarPecas(){
-  
+function MovimentarPecas(localizacaoclick){
+  var butaoTESTE = document.getElementById(localizacaoclick.id)
+  const VerificarExistenciaPeca = butaoTESTE.querySelector('img')
+  console.log('EAEJULIASASAS', VerificarExistenciaPeca)
+  butaoTESTE.style.color = "orange"
 }
 
 GerarTabuleiro()
 ColocarPecasTabuleiro()
+
 //estou organizando o xadrez como se estivesse jogando com peças brancas
+//atributo
