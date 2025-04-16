bc5f8f9c6d6a4c9facf60c3ce6dded9afcd5f46f
Éric Gomes <100045155+ericGL26@users.noreply.github.com>
2025-04-15 23:12:45 -0300

    Add files via upload
    claro que vou refatorar isso kskskksksks

--- xequeMate.js
+++ xequeMate.js
@@ -176,6 +176,132 @@ function CalcularDominioPecas(pecasTabuleiro){
       }
       break;
     case 'cavalo':
+      let bloquearLateralSuperiorEsquerda = false;
+      let bloquearLateralSuperiorDireita = false;
+      let bloquearLateralInferiorEsquerda = false;
+      let bloquearLateralInferiorDireita = false;
+      let bloquearLateralHorizontalEsquerdaCima = false;
+      let bloquearLateralHorizontalEsquerdaBaixo = false;
+      let bloquearLateralHorizontalDireitaCima = false;
+      let bloquearLateralHorizontalDireitaBaixo = false;
+
+      let lateralSuperiorEsquerda = [x1 - 1, parseInt(y1) + 2];
+      let lateralSuperiorDireita = [x1 + 1, parseInt(y1) + 2];
+      let lateralInferiorEsquerda = [x1 - 1, parseInt(y1) - 2];
+      let lateralInferiorDireita = [x1 + 1, parseInt(y1) - 2];
+      let lateralHorizontalEsquerdaCima = [x1 - 2, parseInt(y1) + 1];
+      let lateralHorizontalEsquerdaBaixo = [x1 - 2, parseInt(y1) - 1];
+      let lateralHorizontalDireitaCima = [x1 + 2, parseInt(y1) + 1];
+      let lateralHorizontalDireitaBaixo = [x1 + 2, parseInt(y1) - 1];
+      // filtros
+      lateralSuperiorEsquerda = [lateralSuperiorEsquerda].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralSuperiorDireita = [lateralSuperiorDireita].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralInferiorEsquerda = [lateralInferiorEsquerda].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralInferiorDireita = [lateralInferiorDireita].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralHorizontalEsquerdaCima = [lateralHorizontalEsquerdaCima].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralHorizontalEsquerdaBaixo = [lateralHorizontalEsquerdaBaixo].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralHorizontalDireitaCima = [lateralHorizontalDireitaCima].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      lateralHorizontalDireitaBaixo = [lateralHorizontalDireitaBaixo].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+
+      lateralSuperiorEsquerda = VerificarImpedimento(lateralSuperiorEsquerda);
+      lateralSuperiorDireita = VerificarImpedimento(lateralSuperiorDireita);
+      lateralInferiorEsquerda = VerificarImpedimento(lateralInferiorEsquerda);
+      lateralInferiorDireita = VerificarImpedimento(lateralInferiorDireita);
+      lateralHorizontalEsquerdaCima = VerificarImpedimento(lateralHorizontalEsquerdaCima);
+      lateralHorizontalEsquerdaBaixo = VerificarImpedimento(lateralHorizontalEsquerdaBaixo);
+      lateralHorizontalDireitaCima = VerificarImpedimento(lateralHorizontalDireitaCima);
+      lateralHorizontalDireitaBaixo = VerificarImpedimento(lateralHorizontalDireitaBaixo);
+
+      let buscarPeca = document.getElementById(pecasTabuleiro[pecaVez][0])
+      const timePeca = buscarPeca.querySelector('img')?.name || "semPeca"    
+      
+
+      if(timePeca == 'pecabranca'){
+
+      if (lateralSuperiorEsquerda[0] == "impedimento") {bloquearLateralSuperiorEsquerda = true;}
+      if (bloquearLateralSuperiorEsquerda != true) {
+          casasControleBrancas.push(lateralSuperiorEsquerda);
+      }
+      
+      if (lateralSuperiorDireita[0] == "impedimento") {bloquearLateralSuperiorDireita = true;}
+      if (bloquearLateralSuperiorDireita != true) {
+          casasControleBrancas.push(lateralSuperiorDireita);
+      }
+      
+      if (lateralInferiorEsquerda[0] == "impedimento") {bloquearLateralInferiorEsquerda = true;}
+      if (bloquearLateralInferiorEsquerda != true) {
+          casasControleBrancas.push(lateralInferiorEsquerda);
+      }
+      
+      if (lateralInferiorDireita[0] == "impedimento") {bloquearLateralInferiorDireita = true;}
+      if (bloquearLateralInferiorDireita != true) {
+          casasControleBrancas.push(lateralInferiorDireita);
+      }
+      
+      if (lateralHorizontalEsquerdaCima[0] == "impedimento") {bloquearLateralHorizontalEsquerdaCima = true;}
+      if (bloquearLateralHorizontalEsquerdaCima != true) {
+          casasControleBrancas.push(lateralHorizontalEsquerdaCima);
+      }
+      
+      if (lateralHorizontalEsquerdaBaixo[0] == "impedimento") {bloquearLateralHorizontalEsquerdaBaixo = true;}
+      if (bloquearLateralHorizontalEsquerdaBaixo != true) {
+          casasControleBrancas.push(lateralHorizontalEsquerdaBaixo);
+      }
+      
+      if (lateralHorizontalDireitaCima[0] == "impedimento") {bloquearLateralHorizontalDireitaCima = true;}
+      if (bloquearLateralHorizontalDireitaCima != true) {
+          casasControleBrancas.push(lateralHorizontalDireitaCima);
+      }
+      
+      if (lateralHorizontalDireitaBaixo[0] == "impedimento") {bloquearLateralHorizontalDireitaBaixo = true;}
+      if (bloquearLateralHorizontalDireitaBaixo != true) {
+          casasControleBrancas.push(lateralHorizontalDireitaBaixo);
+      }
+      
+
+      }
+      if(timePeca == 'pecapreta'){
+        if (lateralSuperiorEsquerda[0] == "impedimento") {bloquearLateralSuperiorEsquerda = true;}
+        if (bloquearLateralSuperiorEsquerda != true) {
+          casasContreolePretas.push(lateralSuperiorEsquerda);
+       }
+      
+      if (lateralSuperiorDireita[0] == "impedimento") {bloquearLateralSuperiorDireita = true;}
+      if (bloquearLateralSuperiorDireita != true) {
+        casasContreolePretas.push(lateralSuperiorDireita);
+      }
+      
+      if (lateralInferiorEsquerda[0] == "impedimento") {bloquearLateralInferiorEsquerda = true;}
+      if (bloquearLateralInferiorEsquerda != true) {
+        casasContreolePretas.push(lateralInferiorEsquerda);
+      }
+      
+      if (lateralInferiorDireita[0] == "impedimento") {bloquearLateralInferiorDireita = true;}
+      if (bloquearLateralInferiorDireita != true) {
+        casasContreolePretas.push(lateralInferiorDireita);
+      }
+      
+      if (lateralHorizontalEsquerdaCima[0] == "impedimento") {bloquearLateralHorizontalEsquerdaCima = true;}
+      if (bloquearLateralHorizontalEsquerdaCima != true) {
+        casasContreolePretas.push(lateralHorizontalEsquerdaCima);
+      }
+      
+      if (lateralHorizontalEsquerdaBaixo[0] == "impedimento") {bloquearLateralHorizontalEsquerdaBaixo = true;}
+      if (bloquearLateralHorizontalEsquerdaBaixo != true) {
+        casasContreolePretas.push(lateralHorizontalEsquerdaBaixo);
+      }
+      
+      if (lateralHorizontalDireitaCima[0] == "impedimento") {bloquearLateralHorizontalDireitaCima = true;}
+      if (bloquearLateralHorizontalDireitaCima != true) {
+          casasContreolePretas.push(lateralHorizontalDireitaCima);
+      }
+      
+      if (lateralHorizontalDireitaBaixo[0] == "impedimento") {bloquearLateralHorizontalDireitaBaixo = true;}
+      if (bloquearLateralHorizontalDireitaBaixo != true) {
+        casasContreolePretas.push(lateralHorizontalDireitaBaixo);
+      }
+      
+      }
       break;
     case 'bispo':
       // Flags que controlam o push de casas livres ou impedidas; ao encontrar um impedimento, param a adição sem precisar de vários ifs ou loops. false = nao bloqueia, mudei para let antes era var se der erro provavelmente é por isso
@@ -344,11 +470,181 @@ function CalcularDominioPecas(pecasTabuleiro){
         }
         if(timePeca == 'pecapreta'){
 
+          if(diagonalSuperiorEsquerdaRainha[0] == "impedimento"){bloqueiaDiagonalSuperiorEsquerdaRainha = true}
+          if(bloqueiaDiagonalSuperiorEsquerdaRainha != true){
+           casasContreolePretas.push(diagonalSuperiorEsquerdaRainha)
+         }
+
+         if(diagonalSuperiorDireitaRainha[0] == "impedimento"){bloqueiaDiagonalSuperiorDireitaRainha = true}
+          if(bloqueiaDiagonalSuperiorDireitaRainha != true){
+            casasContreolePretas.push(diagonalSuperiorDireitaRainha)
+         }
+
+         if(diagonalInferiorEsquerdaRainha[0] == "impedimento") {bloqueiaDiagonalInferiorEsquerdaRainha = true}
+        if(bloqueiaDiagonalInferiorEsquerdaRainha != true) {
+          casasContreolePretas.push(diagonalInferiorEsquerdaRainha);
+        }
+
+         if(diagonalInferiorDireitaRainha[0] == "impedimento"){bloqueiaDiagonalInferiorDireitaRainha = true}
+         if(bloqueiaDiagonalInferiorDireitaRainha != true){
+          casasContreolePretas.push(diagonalInferiorDireitaRainha)
+         }
+        
+         //VERTICAL E HORIZONTAL:
+
+         if(horizontalEsquerdaRainha[0] == "impedimento"){bloqueiaEsquerdaRainha = true}
+           if(bloqueiaEsquerdaRainha != true){
+            casasContreolePretas.push(horizontalEsquerdaRainha)
+          }
+
+          if(horizontalDireitaRainha[0] == "impedimento"){bloqueiaDireitaRainha = true}
+           if(bloqueiaDireitaRainha != true){
+            casasContreolePretas.push(horizontalDireitaRainha)
+          }
+
+          if(verticalCimaRainha[0] == "impedimento"){bloqueiaCimaRainha = true}
+          if(bloqueiaCimaRainha != true){
+            casasContreolePretas.push(verticalCimaRainha)
+          }
+
+          if(verticalBaixoRainha[0] == "impedimento"){bloqueiaBaixoRainha = true}
+          if(bloqueiaBaixoRainha != true){
+            casasContreolePretas.push(verticalBaixoRainha)
+          }
+
+
         }
       }
       break;
     case 'rei':
-      break;
+      let bloquearHorizontalEsquerda = false;
+      let bloquearHorizontalDireita = false;
+      let bloquearVerticalCima = false;
+      let bloquearVerticalBaixo = false;
+      let bloquearDiagonalSuperiorEsquerda = false;
+      let bloquearDiagonalSuperiorDireita = false;
+      let bloquearDiagonalInferiorEsquerda = false;
+      let bloquearDiagonalInferiorDireita = false;
+
+    for(let contador = 1; contador < 9; contador++){
+      let horizontalEsquerda = [x1 - 1, parseInt(y1)];
+      let horizontalDireita = [x1 + 1, parseInt(y1)];
+      let verticalCima = [x1, parseInt(y1) + 1];
+      let verticalBaixo = [x1, parseInt(y1) - 1];
+      let diagonalSuperiorEsquerda = [x1 - 1, parseInt(y1) + 1];
+      let diagonalSuperiorDireita = [x1 + 1, parseInt(y1) + 1];
+      let diagonalInferiorEsquerda = [x1 - 1, parseInt(y1) - 1];
+      let diagonalInferiorDireita = [x1 + 1, parseInt(y1) - 1];
+      //filtros
+      horizontalEsquerda = [horizontalEsquerda].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      horizontalDireita = [horizontalDireita].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      verticalCima = [verticalCima].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      verticalBaixo = [verticalBaixo].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      diagonalSuperiorEsquerda = [diagonalSuperiorEsquerda].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      diagonalSuperiorDireita = [diagonalSuperiorDireita].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      diagonalInferiorEsquerda = [diagonalInferiorEsquerda].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+      diagonalInferiorDireita = [diagonalInferiorDireita].filter(([x, y]) => x > 0 && x <= 8 && y > 0 && y <= 8)[0] || null;
+     
+      horizontalEsquerda = VerificarImpedimento(horizontalEsquerda);
+      horizontalDireita = VerificarImpedimento(horizontalDireita);
+      verticalCima = VerificarImpedimento(verticalCima);
+      verticalBaixo = VerificarImpedimento(verticalBaixo);
+      diagonalSuperiorEsquerda = VerificarImpedimento(diagonalSuperiorEsquerda);
+      diagonalSuperiorDireita = VerificarImpedimento(diagonalSuperiorDireita);
+      diagonalInferiorEsquerda = VerificarImpedimento(diagonalInferiorEsquerda);
+      diagonalInferiorDireita = VerificarImpedimento(diagonalInferiorDireita);
+
+      let buscarPeca = document.getElementById(pecasTabuleiro[pecaVez][0])
+      const timePeca = buscarPeca.querySelector('img')?.name || "semPeca"
+
+      if(timePeca == 'pecabranca'){
+
+        if (horizontalEsquerda[0] == "impedimento") {bloquearHorizontalEsquerda = true}
+        if (bloquearHorizontalEsquerda != true) {
+        casasControleBrancas.push(horizontalEsquerda)
+        }
+
+        if (horizontalDireita[0] == "impedimento") {bloquearHorizontalDireita = true}
+        if (bloquearHorizontalDireita != true) {
+          casasControleBrancas.push(horizontalDireita)
+        }
+
+        if (verticalCima[0] == "impedimento") {bloquearVerticalCima = true}
+        if (bloquearVerticalCima != true) {
+          casasControleBrancas.push(verticalCima)
+        }
+
+        if (verticalBaixo[0] == "impedimento") {bloquearVerticalBaixo = true}
+        if (bloquearVerticalBaixo != true) {
+          casasControleBrancas.push(verticalBaixo)
+        }
+
+        if (diagonalSuperiorEsquerda[0] == "impedimento") {bloquearDiagonalSuperiorEsquerda = true}
+        if (bloquearDiagonalSuperiorEsquerda != true) {
+          casasControleBrancas.push(diagonalSuperiorEsquerda)
+        }
+
+        if (diagonalSuperiorDireita[0] == "impedimento") {bloquearDiagonalSuperiorDireita = true}
+        if (bloquearDiagonalSuperiorDireita != true) {
+          casasControleBrancas.push(diagonalSuperiorDireita)
+        }
+
+        if (diagonalInferiorEsquerda[0] == "impedimento") {bloquearDiagonalInferiorEsquerda = true}
+        if (bloquearDiagonalInferiorEsquerda != true) {
+          casasControleBrancas.push(diagonalInferiorEsquerda)
+        }
+
+        if (diagonalInferiorDireita[0] == "impedimento") {bloquearDiagonalInferiorDireita = true}
+        if (bloquearDiagonalInferiorDireita != true) {
+          casasControleBrancas.push(diagonalInferiorDireita)
+        }
+
+
+      }
+      if(timePeca == 'pecapreta'){
+        if (horizontalEsquerda[0] == "impedimento") {bloquearHorizontalEsquerda = true}
+        if (bloquearHorizontalEsquerda != true) {
+          casasContreolePretas.push(horizontalEsquerda)
+        }
+        
+        if (horizontalDireita[0] == "impedimento") {bloquearHorizontalDireita = true}
+        if (bloquearHorizontalDireita != true) {
+          casasContreolePretas.push(horizontalDireita)
+        }
+        
+        if (verticalCima[0] == "impedimento") {bloquearVerticalCima = true}
+        if (bloquearVerticalCima != true) {
+          casasContreolePretas.push(verticalCima)
+        }
+        
+        if (verticalBaixo[0] == "impedimento") {bloquearVerticalBaixo = true}
+        if (bloquearVerticalBaixo != true) {
+          casasContreolePretas.push(verticalBaixo)
+        }
+        
+        if (diagonalSuperiorEsquerda[0] == "impedimento") {bloquearDiagonalSuperiorEsquerda = true}
+        if (bloquearDiagonalSuperiorEsquerda != true) {
+          casasContreolePretas.push(diagonalSuperiorEsquerda)
+        }
+        
+        if (diagonalSuperiorDireita[0] == "impedimento") {bloquearDiagonalSuperiorDireita = true}
+        if (bloquearDiagonalSuperiorDireita != true) {
+          casasContreolePretas.push(diagonalSuperiorDireita)
+        }
+        
+        if (diagonalInferiorEsquerda[0] == "impedimento") {bloquearDiagonalInferiorEsquerda = true}
+        if (bloquearDiagonalInferiorEsquerda != true) {
+          casasContreolePretas.push(diagonalInferiorEsquerda)
+        }
+        
+        if (diagonalInferiorDireita[0] == "impedimento") {bloquearDiagonalInferiorDireita = true}
+        if (bloquearDiagonalInferiorDireita != true) {
+          casasContreolePretas.push(diagonalInferiorDireita)
+        }
+        
+      }
+    }
+     break;
   }
  }
 
